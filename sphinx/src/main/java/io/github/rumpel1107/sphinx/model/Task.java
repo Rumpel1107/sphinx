59961c858c4eb135a41d6914ef282db0029516a4
Rumpel <ing.mauriciomedina@gmail.com>
2025-06-24 22:13:39 -0500

    feat: Create data model for BaseItem and Task

---
+++ sphinx/src/main/java/io/github/rumpel1107/sphinx/model/Task.java
@@ -0,0 +1,17 @@
+package io.github.rumpel1107.sphinx.model;
+
+public class Task extends BaseItem {
+
+	private String status;
+
+	// Getter for the 'status' property
+	public String getStatus() {
+
+		return status;
+	}
+
+	public void setStatus(String status) {
+
+		this.status = status;
+	}
+}
